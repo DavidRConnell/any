9894afc39f8a90ec72b5fe75c9cfaa6bdb1bcb4f
David R Connell <davidconnell12@gmail.com>
2020-03-07 14:52:42 -0600

    Clean: take advantage of helper functions
    Simplify by offshoring checks and selection to RANDOMLYSAMPLEWITHOUTREPLACEMENT

--- fileIn.m
+++ fileIn.m
@@ -1,8 +1,12 @@
 function filename = fileIn(dir, numFilesDesired)
 %FILESIN randomly return any random file in a directory
 %   FILENAME = FILEIN(DIRECTORY) return a randomly selected file from DIRECTORY.
+%
 %   FILELIST = FILEIN(DIRECTORY, N) return a list of N files randomly
 %   selected (without replacement) from DIRECTORY.
+%
+%   FILELIST = FILEIN(DIRECTORY, [MINN, MAXN]) randomly select length of
+%   returned FILELIST between MINN and MAXN.
 %
 %   See also +UTILS/LISTFILES
 
@@ -10,14 +14,17 @@ function filename = fileIn(dir, numFilesDesired)
         numFilesDesired = 1;
     end
 
-    allFilesInDir = utils.listFiles(dir);
-    numFilesInDir = length(allFilesInDir);
-    validateattributes(numFilesDesired, {'numeric'}, {'positive', 'integer'})
-    assert(numFilesDesired <= numFilesInDir, ...
-           'Any:AskedForTooManyFiles', ...
-           'Asked for more files than exist in "%s" (%d total)', dir, numFilesInDir)
-
-    filename = randomlySampleWithoutReplacement(allFilesInDir, numFilesDesired);
+    try
+        filename = randomlySampleWithoutReplacement(utils.listFiles(dir), ...
+                                                    numFilesDesired);
+    catch ME
+        if strcmp(ME.identifier, 'MATLAB:randperm:inputKTooLarge')
+            error('Any:AskedForTooManyFiles', ...
+                  'Asked for more files than exist in "dir"', dir)
+        else
+            rethrow(ME)
+        end
+    end
 
     if numFilesDesired == 1
         filename = filename{1};
